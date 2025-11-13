76d65a2838dc652ac2113455234681fff94548ca
SBrev30 <sheldonbrev@hotmail.com>
2025-11-13 00:31:27 -0500

    Update Sidebar.jsx

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import useNotion from './hooks/useNotion';
+import { useNotion } from '../../hooks/useNotion';
 
 /**
  * Example Sidebar Component
